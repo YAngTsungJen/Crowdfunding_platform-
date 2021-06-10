2e30885dcdcec3be5116014a53d5647a85938a2f
mike <q23362352@yhaoo.com.tw>
2021-06-10 23:13:36 +0800

    測試

--- js/all.js
+++ js/all.js
@@ -1,8 +1,10 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// enable tooltips
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
 // form驗證
 (function () {
   "use strict";
